e479e3d1c7428f7db30733fc84a1c4140c68b506
ManjuA <manjua@udel.edu>
2023-07-11 06:01:52 -0400

    changes to include ser-thr-atlas assessment in plot

--- miscellaneous/src/plot_curves.py
+++ miscellaneous/src/plot_curves.py
@@ -12,7 +12,7 @@ class PerformanceDataSets:
 
     def plot_roc_curve():
         plt.close('all')
-        fig, ax = plt.subplots(figsize=(7.5, 7.5))
+        fig, ax = plt.subplots(figsize=(9, 9))
         assess3 = Assessment(g_constants.ASSESS3_DATA_PATH)
         assess3.run()
         y_labels, y_probs = assess3.y_labels, assess3.y_probs
@@ -33,23 +33,22 @@ class PerformanceDataSets:
         y_labels, y_probs = assess6.y_labels, assess6.y_probs
         roc_score6, fpr6, tpr6, _ = Score.get_roc_scores(y_labels, y_probs)
 
-        plt.plot(fpr3, tpr3, color='red', label='KSFinder dataset (AUC = %0.3f)' % roc_score3)
-        plt.plot(fpr6, tpr6, color='orange', label='PredKinKG dataset (AUC = %0.3f)' % roc_score6)
-        plt.plot(fpr4, tpr4, label='PredKinKG-B (AUC = %0.3f)' % roc_score4)
-        plt.plot(fpr5, tpr5, color='green', label='LinkPhinder dataset (AUC = %0.3f)' % roc_score5)
-        
+        plt.plot(fpr3, tpr3, color='red', label='Assessment 3 - KSFinder dataset (AUC = %0.3f)' % roc_score3)
+        plt.plot(fpr4, tpr4, label='Assessment 4 - PredKinKG-B dataset (AUC = %0.3f)' % roc_score4)
+        plt.plot(fpr5, tpr5, color='green', label='Assessment 5 - LinkPhinder dataset (AUC = %0.3f)' % roc_score5)
+        plt.plot(fpr6, tpr6, color='orange', label='Assessment 6 - PredKinKG dataset (AUC = %0.3f)' % roc_score6)
 
-        plt.plot([0, 1], [0, 1], linestyle='--', color='blue', label='Random classifier')  
+        #plt.plot([0, 1], [0, 1], linestyle='--', color='blue', label='Random classifier')  
         plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='green', label='Perfect classifier')
         plt.xlabel('False positive rate')
         plt.ylabel('True positive rate')
-        plt.legend(loc=(0.4,0.1))
+        plt.legend(loc=(0.25,0.1))
 
         plt.savefig(g_constants.MISC_OUTPUT+'comparative_dataset_roc.png',dpi=400)
 
     def plot_pr_curve():
         plt.close('all')
-        fig, ax = plt.subplots(figsize=(7.5, 7.5))
+        fig, ax = plt.subplots(figsize=(9, 9))
         assess3 = Assessment(g_constants.ASSESS3_DATA_PATH)
         assess3.run()
         y_labels, y_probs = assess3.y_labels, assess3.y_probs
@@ -70,16 +69,16 @@ class PerformanceDataSets:
         y_labels, y_probs = assess6.y_labels, assess6.y_probs
         pr_score6, precision6, recall6, _ = Score.get_pr_scores(y_labels, y_probs)
 
-        plt.plot(recall3, precision3, color='red', label='KSFinder dataset (Avg PR = %0.3f)' % pr_score3)
-        plt.plot(recall6, precision6, color='orange', label='PredKinKG dataset (Avg PR = %0.3f)' % pr_score6)
-        plt.plot(recall4, precision4, label='PredKinKG-B (Avg PR = %0.3f)' % pr_score4)
-        plt.plot(recall5, precision5, color='green', label='LinkPhinder dataset (Avg PR = %0.3f)' % pr_score5)
+        plt.plot(recall3, precision3, color='red', label='Assessment 3 - KSFinder dataset (Avg PR = %0.3f)' % pr_score3)
+        plt.plot(recall4, precision4, label='Assessment 4 - PredKinKG-B dataset (Avg PR = %0.3f)' % pr_score4)
+        plt.plot(recall5, precision5, color='green', label='Assessment 5 -LinkPhinder dataset (Avg PR = %0.3f)' % pr_score5)
+        plt.plot(recall6, precision6, color='orange', label='Assessment 6 - PredKinKG dataset (Avg PR = %0.3f)' % pr_score6)
 
-        plt.plot([0, 0, 1], [0, 0, 0], linestyle='--', color='blue', label='Random classifier')  
+        #plt.plot([0, 0, 1], [0, 0, 0], linestyle='--', color='blue', label='Random classifier')  
         plt.plot([0, 1, 1], [1, 1, 0], linestyle=':', color='green', label='Perfect classifier')
         plt.xlabel('recall')
         plt.ylabel('precision')
-        plt.legend(loc=(0.4,0.1))
+        plt.legend(loc=(0.2,0.1))
 
         plt.savefig(g_constants.MISC_OUTPUT+'comparative_dataset_pr.png',dpi=400)
 
@@ -96,7 +95,7 @@ class CompareModels:
 
     def plot_roc_curve():
         plt.close('all')
-        fig, ax = plt.subplots(figsize=(7.5, 7.5))
+        fig, ax = plt.subplots(figsize=(9, 9))
         assess3 = Assessment(g_constants.ASSESS3_DATA_PATH)
         assess3.run()
         y_labels, y_probs = assess3.y_labels, assess3.y_probs
@@ -112,22 +111,27 @@ class CompareModels:
         y_labels, y_probs = assess8.y_labels, assess8.y_probs
         roc_score8, fpr8, tpr8, _ = Score.get_roc_scores(y_labels, y_probs)
 
-        plt.plot(fpr3, tpr3, color='red', label='KSFinder (AUC = %0.3f)' % roc_score3)
-        plt.plot(fpr7, tpr7, color='orange', label='LinkPhinder (AUC = %0.3f)' % roc_score7)
-        plt.plot(fpr8, tpr8, label='PredKinKG (AUC = %0.3f)' % roc_score8)
-        
+        assess9 = compare_assess(g_constants.ASSESS9_DATA_PATH)
+        assess9.run()
+        y_labels, y_probs = assess9.y_labels, assess9.y_probs
+        roc_score9, fpr9, tpr9, _ = Score.get_roc_scores(y_labels, y_probs)
 
-        plt.plot([0, 1], [0, 1], linestyle='--', color='blue', label='Random classifier')  
+        plt.plot(fpr3, tpr3, color='red', label='Assessment 3 - KSFinder (AUC = %0.3f)' % roc_score3)
+        plt.plot(fpr7, tpr7, color='orange', label='Assessment 7 - LinkPhinder (AUC = %0.3f)' % roc_score7)
+        plt.plot(fpr8, tpr8, label='Assessment 8 - PredKinKG (AUC = %0.3f)' % roc_score8)
+        plt.plot(fpr9, tpr9, color='green', label='Assessment 9 - Ser-Thr-KS atlas (AUC = %0.3f)' % roc_score9)
+        
+        # plt.plot([0, 1], [0, 1], linestyle='--', color='blue', label='Random classifier')  
         plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='green', label='Perfect classifier')
         plt.xlabel('False positive rate')
         plt.ylabel('True positive rate')
-        plt.legend(loc=(0.4,0.1))
+        plt.legend(loc=(0.25,0.1))
 
         plt.savefig(g_constants.MISC_OUTPUT+'comparative_models_roc.png',dpi=400)
 
     def plot_pr_curve():
         plt.close('all')
-        fig, ax = plt.subplots(figsize=(7.5, 7.5))
+        fig, ax = plt.subplots(figsize=(9, 9))
         assess3 = Assessment(g_constants.ASSESS3_DATA_PATH)
         assess3.run()
         y_labels, y_probs = assess3.y_labels, assess3.y_probs
@@ -143,15 +147,21 @@ class CompareModels:
         y_labels, y_probs = assess8.y_labels, assess8.y_probs
         pr_score8, precision8, recall8, _ = Score.get_pr_scores(y_labels, y_probs)
 
-        plt.plot(recall3, precision3, color='red', label='KSFinder (Avg PR = %0.3f)' % pr_score3)
-        plt.plot(recall7, precision7, color='orange', label='LinkPhinder (Avg PR = %0.3f)' % pr_score7)
-        plt.plot(recall8, precision8, label='PredKinKG (Avg PR = %0.3f)' % pr_score8)
+        assess9 = compare_assess(g_constants.ASSESS9_DATA_PATH)
+        assess9.run()
+        y_labels, y_probs = assess9.y_labels, assess9.y_probs
+        pr_score9, precision9, recall9, _ = Score.get_pr_scores(y_labels, y_probs)
 
-        plt.plot([0, 0, 1], [0, 0, 0], linestyle='--', color='blue', label='Random classifier')  
+        plt.plot(recall3, precision3, color='red', label='Assessment 3 - KSFinder (Avg PR = %0.3f)' % pr_score3)
+        plt.plot(recall7, precision7, color='orange', label='Assessment 7 - LinkPhinder (Avg PR = %0.3f)' % pr_score7)
+        plt.plot(recall8, precision8, label='Assessment 8 - PredKinKG (Avg PR = %0.3f)' % pr_score8)
+        plt.plot(recall9, precision9, color='green', label='Assessment 9 - Ser-Thr-KS atlas (AUC = %0.3f)'  % pr_score9)
+
+        # plt.plot([0, 0, 1], [0, 0, 0], linestyle='--', color='blue', label='Random classifier')  
         plt.plot([0, 1, 1], [1, 1, 0], linestyle=':', color='green', label='Perfect classifier')
         plt.xlabel('recall')
         plt.ylabel('precision')
-        plt.legend(loc=(0.4,0.1))
+        plt.legend(loc=(0.25,0.1))
 
         plt.savefig(g_constants.MISC_OUTPUT+'comparative_models_pr.png',dpi=400)
 
